2ac8421aa3332ea6a982500ae4327fb4d8ff3bec
1234renuka <146978415+1234renuka@users.noreply.github.com>
2024-11-04 13:22:14 +0530

    Add files via upload

---
+++ class.c
@@ -0,0 +1,88 @@
+/*#include<stdio.h>
+
+void myfunction(){
+    printf("I got just executed !");
+}
+int main(){
+    myfunction();//call the function
+    
+    return 0;
+
+    
+}*/
+
+/*#include<stdio.h>
+void mydate(){
+    printf("Today Date I dont know");
+
+}
+int main(){
+    mydate();
+    return 0;
+}*/
+
+/*include<stdio.h>
+void myname(char name[]){
+    printf("hello %s\n",name);
+
+}
+int main(){
+    myname("Renuka");
+    myname("Sonali");
+    myname("khushi");
+    myname("shanu");
+    return 0;
+}*/
+
+/*#include<stdio.h>
+void mynameage(char name[],int age){
+    printf("hello %s.your age is %d am i right \n ",name,age);
+}
+
+int main(){
+    mynameage("REnuka",22);
+    mynameage("prafull",22);
+    return 0;
+}*/
+/*#include<stdio.h>
+void myarray(int mynumbers[5]){
+    for(int i=0; i<5; i++){
+        printf("%d\n",mynumbers[i]);
+    }
+
+}
+int main(){
+    int mynumbers[5]={ 10,20,30,40,50};
+    myarray(mynumbers);
+
+    return 0;
+}*/
+
+/*#include<stdio.h>
+int mydump(int x){
+    return 5*x;
+}
+int main(){
+    printf("result : %d",mydump(12345));
+    return 0;
+}*/
+/*#include<stdio.h>
+int mydumpo(int x,int y,float z){
+    return x*y/z;
+}
+int main(){
+    printf("Result is: %d",mydumpo(2344,564,678));//float liya %f to 0.000 value ayi or jb int liya %d to 1949 value ayi dono pr alg value
+    return 0;
+}*/
+
+#include<stdio.h>
+#include<math.h>
+
+/*int main(){
+    printf("%f",sqrt(256));
+    return 0;
+}/*
+
+
+
+
